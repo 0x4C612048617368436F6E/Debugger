0d02ccac4ba4e8a96b8b41b7a303bb1ee360caa7
Benjamin <benjaminenadeba@gmail.com>
2025-08-31 20:55:44 +0100

    Get the symbol table ....

---
+++ hold.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(int argc, char** argv){
+    printf("\nThis is our test program\n");
+    return 0;
+}
--- main.c
+++ main.c
@@ -3,8 +3,10 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<sys/ptrace.h>
+#include<sys/wait.h>
 
-void debugger();
+void debugger(char** argv);
+int getInput(char* input);
 
 int main(int argc, char** argv){
     //Will allow used to include the compiled program (In debug mode. For now, only 2 arguments allowed)
@@ -12,9 +14,11 @@ int main(int argc, char** argv){
         printf("Number of Arguments is to little or too much\n");
         return -1;
     }
+    //printf("Welcome to this simple debuger");
     debugger(argv);
     return 0;
 }
+
 
 void debugger(char** argv){
     pid_t processCreate = fork();
@@ -24,22 +28,29 @@ void debugger(char** argv){
     }else if(processCreate == 0){
         //child process created
         pid_t childProcessID = getpid();
-       //rintf("Child process ID is: %i\n",childProcessID);
-        /**
-    * Will make use of the ptrace system call which provides the mechansim for one process to trace a child process
-    * 
-    */
-   if(ptrace(PTRACE_TRACEME,0,NULL,NULL) == -1){
-    printf("ptrace system call failed\n");
-    exit(-1);
-   }
-
-   execl(argv[1],argv[1],NULL);
+        if(ptrace(PTRACE_TRACEME,0,NULL,NULL) == -1){
+            printf("ptrace system call failed\n");
+            exit(-1);
+        }
+        if(execl(argv[1],argv[1],NULL) == -1){
+            printf("Error occured");
+            exit(-1);
+        }
         
     }else{
         //parent process
-        pid_t parentProcessID = getpid();
-       //rintf("Parent process ID is: %i\n",parentProcessID);
+        //pid_t parentProcessID = getpid();
+        printf("For help, type \"help\".\n");
+        printf("(No debugging symbols found in ./test)\n");
+        int status;
+        waitpid(processCreate, &status, 0);
+        while(1){
+            //ptrace(PTRACE_CONT, processCreate, NULL, NULL);
+            waitpid(processCreate, &status, 0);
+        }
     }
 }
 
+int getInput(char* input){
+
+}
